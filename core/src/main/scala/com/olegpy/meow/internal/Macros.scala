b55efd2f383df4e7fe78e13ad430af9a45a97f41
Mikołaj Robakowski <mikolaj.rob@gmail.com>
2021-06-27 22:23:47 +0200

    remove unnecessary .typeConstructor

--- core/src/main/scala/com/olegpy/meow/internal/Macros.scala
+++ core/src/main/scala/com/olegpy/meow/internal/Macros.scala
@@ -8,13 +8,14 @@ import scala.reflect.macros.whitebox
 import scala.language.experimental.macros
 
 object Macros {
+
   def deriveAsk[F[_], A](c: whitebox.Context): c.Expr[Ask[F, A]] = {
     import c.universe._
 
     if (c.enclosingMacros.size > 1) abortExpansion("unsupported recursive call")
 
     val ask = c.openImplicits.head.pt
-    val invariantAsk = makeInvariant(c)(ask)
+    val invariantAsk = appliedType(implicitly[c.WeakTypeTag[AskOptics.Invariant[Id, Any]]].tpe, ask.typeArgs)
 
     val foundImplicit = c.inferImplicitValue(invariantAsk)
     if (foundImplicit.isEmpty) abortExpansion("found no suitable implicits")
@@ -25,16 +26,6 @@ object Macros {
 
     c.Expr[Ask[F, A]](q"$foundImplicit.value")
   }
-
-  def makeInvariant(c: whitebox.Context)(ask: c.Type): c.Type = {
-    import c.universe._
-
-    val invAsk = implicitly[c.WeakTypeTag[AskOptics.Invariant[Id, Any]]].tpe.typeConstructor
-    val args = ask.typeArgs
-
-    appliedType(invAsk, args)
-  }
-
 
   // I don't think this is ever seen by the user when compiling, just here for code clarity...
   private def abortExpansion(message: String = "<no message>"): Nothing = throw new RuntimeException(s"aborting macro expansion: $message")
